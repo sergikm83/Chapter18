cc52d137d509f75129f2dcd4d76ee76431d9e25c
Sergey Misko <spy_3462@mail.ru>
2022-07-28 16:40:12 +0400

    добавил метод CreateMyAsm, в котором создается  динамическая сборка с классом HelloWorld

--- MyAsmBuilder/Program.cs
+++ MyAsmBuilder/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Reflection.Emit;
+using System.Reflection;
+using System.Threading;
 
 namespace MyAsmBuilder
 {
@@ -7,6 +10,36 @@ namespace MyAsmBuilder
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+        // Объект AppDomain отправляется вызывающим кодом.
+        public static void CreateMyAsm(AppDomain curAppDomain)
+        {
+            // Установить общие характеристики сборки.
+            AssemblyName assemblyName = new AssemblyName();
+            assemblyName.Name = "MyAssembly";
+            assemblyName.Version = new Version("1.0.0.0");
+            // Создать новую сборку внутри текущего домена приложения.
+            AssemblyBuilder assembly = AssemblyBuilder.DefineDynamicAssembly(assemblyName, AssemblyBuilderAccess.RunAndCollect);
+            // Поскольку строится однофайловая сборка, имя модуля
+            // будет таким же, как у сборки.
+            ModuleBuilder module = assembly.DefineDynamicModule("MyAssembly");
+            // Определить открытый класс по имени HelloWorld.
+            TypeBuilder helloWorldClass = module.DefineType("MyAssembly.HelloWorld", TypeAttributes.Public);
+            // Определить закрытыю переменную-член типа String по имени theMessage.
+            FieldBuilder msgField = helloWorldClass.DefineField("theMessage", Type.GetType("System.String"), FieldAttributes.Private);
+            // Создать специальный конструктор.
+            Type[] constructorArgs = new Type[1];
+            constructorArgs[0] = typeof(string);
+            ConstructorBuilder constructor = helloWorldClass.DefineConstructor(MethodAttributes.Public, CallingConventions.Standard, constructorArgs);
+            ILGenerator constructorIL = constructor.GetILGenerator();
+            constructorIL.Emit(OpCodes.Ldarg_0);
+            Type objectClass = typeof(object);
+            ConstructorInfo superConstructor = objectClass.GetConstructor(new Type[0]);
+            constructorIL.Emit(OpCodes.Call, superConstructor);
+            constructorIL.Emit(OpCodes.Ldarg_0);
+            constructorIL.Emit(OpCodes.Ldarg_1);
+            constructorIL.Emit(OpCodes.Stfld,msgField);
+            constructorIL.Emit(OpCodes.Ret);
         }
     }
 }
